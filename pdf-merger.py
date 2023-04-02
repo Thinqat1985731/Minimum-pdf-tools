f77cf8c521cd7fbaac56dc4108efb842581f10c2
shingo <kata.shin.2001@gmail.com>
2023-04-03 03:49:26 +0900

    fix: various points

--- pdf-merger.py
+++ pdf-merger.py
@@ -1,6 +1,6 @@
-import os
+import os, sys
 from tkinter import messagebox, filedialog, Tk
-from pypdf import PdfWriter, PdfReader
+from pypdf import PdfWriter
 
 dirname = os.path.dirname(__file__) 
 iDir = os.path.abspath(dirname) 
@@ -14,30 +14,37 @@ files_read = filedialog.askopenfilenames(
 
 files_found = ''
 for file_name in files_read:
-    files_found = files_found + file_name + '\n' # 取り敢えず確認だけ取る
+    files_found = files_found + file_name + '\n'
 
-yes_no = messagebox.askquestion('Pdf-merger', '以下の' + str(len(files_read)) + 
-                                '個のファイルを結合します：\n' + files_found + 
-                                '続行しますか?')
+if files_found != '': # ファイルが存在する場合
+    yes_no = messagebox.askquestion(
+        'Pdf-merger', '以下の' + str(len(files_read)) + '個のファイルを結合します：\n' + 
+        files_found + '続行しますか?')
 
-if yes_no == 'yes':
-    pdf_file_merger = PdfWriter()
-    for pdf in files_read:
-        pdf_file_merger.append(pdf)
+    if yes_no == 'yes':
+        pdf_file_merger = PdfWriter()
+        for pdf in files_read:
+            pdf_file_merger.append(pdf)
 
-    file_name_save = filedialog.asksaveasfilename(
-        title='結合したファイルを名前を付けて保存',
-        filetypes=[("PDF file","*.pdf")], 
-        initialdir=iDir)
-    if file_name_save.rfind('.pdf') == -1:
-        file_name_save = file_name_save + '.pdf' 
-        # 右から検索して.pdfが無かったら勝手に付け足す
+        file_name_save = filedialog.asksaveasfilename(
+            title='結合したファイルを名前を付けて保存',
+            filetypes=[("PDF file","*.pdf")], 
+            initialdir=iDir)
+        if file_name_save.rfind('.pdf') == -1:
+            file_name_save = file_name_save + '.pdf' 
+            # 右から検索して.pdfが無かったら勝手に付け足す
     
-    pdf_file_merger.write(file_name_save)
-    pdf_file_merger.close() # writer を閉じる
-    messagebox.showinfo('Pdf-merger', '処理が完了しました。')
-    root.destroy()
+        pdf_file_merger.write(file_name_save)
+        pdf_file_merger.close() # writer を閉じる
+        messagebox.showinfo('Pdf-merger', '処理が完了しました。')
+        root.destroy()
+
+    else:
+        messagebox.showinfo('Pdf-merger', 'キャンセルされました。\n最初からやり直してください。')
+        root.destroy()
+        sys.exit()
 
 else:
-    messagebox.showinfo('Pdf-merger', 'キャンセルされました。\n最初からやり直してください。')
+    messagebox.showerror('Pdf-merger', 'データが選択されていません。\n最初からやり直してください。')
     root.destroy()
+    sys.exit()
--- readme.md
+++ readme.md
@@ -1,3 +1,23 @@
 # Pdf-tools
-pypdfを使ってみたかっただけのガバガバツールです（初期はPyPDF2を検討しておりましたが、[PyPDF2のPypiサイト](https://pypi.org/project/PyPDF2/)に
-よれば「v3.0.X（2023年12月31日リリース）で開発を停止してルーツであるpypdfで開発を続ける」とのことだったので、pypdfを利用しています）。
+
+## Overview
+pypdfを使ってみたかっただけのガバガバツールです（初期はPyPDF2を検討しておりましたが、[PyPDF2のPypiサイト](https://pypi.org/project/PyPDF2/)によれば「v3.0.X（23/12/31リリース）で開発を停止してルーツであるpypdfで開発を続ける」とのことだったので、pypdfを利用しています）。
+
+## Requirement
+- pypdf
+
+## Usage
+Tkinterで何とかダイアログ化したので見れば分かると思います。
+
+## Features
+データが無いときに警告したり、pdf-mergerで保存時に.pdf付け忘れた場合に勝手に付け足すくらいしかないです。あとは巷に転がっている感じのやつ。
+
+## Author
+<div style="text-align: center;">
+<img src="https://avatars.githubusercontent.com/u/113882060?v=4" width="20%"><br>
+<strong>Thinqat(Thinqat1985731)</strong>
+</div>
+
+## History
+2023/4/2　公開<br>
+2023/4/3　データの無いときの処理・Python自体の終了処理追加<br>
